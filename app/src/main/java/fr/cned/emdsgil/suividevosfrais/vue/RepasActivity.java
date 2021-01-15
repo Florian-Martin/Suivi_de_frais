d5c4716e04609a80ac0283097cd488f5afeed913
Florian MARTIN <florian.martin7469@gmail.com>
2021-01-15 20:09:14 +0100

    Poursuite du passage en MVC : classe RepasActivity traitée. Saisie et enregistrement de frais dans RepasActivity désormais possible.

--- app/src/main/java/fr/cned/emdsgil/suividevosfrais/vue/RepasActivity.java
+++ app/src/main/java/fr/cned/emdsgil/suividevosfrais/vue/RepasActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,15 +16,18 @@ import android.widget.ImageView;
 import java.util.Locale;
 
 import fr.cned.emdsgil.suividevosfrais.R;
+import fr.cned.emdsgil.suividevosfrais.controleur.Controleur;
+import fr.cned.emdsgil.suividevosfrais.modele.FraisMois;
 import fr.cned.emdsgil.suividevosfrais.outils.Global;
 import fr.cned.emdsgil.suividevosfrais.outils.Outils;
+import fr.cned.emdsgil.suividevosfrais.outils.Serializer;
 
 public class RepasActivity extends AppCompatActivity {
 
     // -------- VARIABLES --------
-    private Integer annee ;
-    private Integer mois ;
-    private Integer qte ;
+    private Controleur controleur;
+    private DatePicker datePicker;
+    private final String typeFrais = "repas";
 
 
     // -------- CYCLE DE VIE --------
@@ -32,10 +36,18 @@ public class RepasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repas);
         setTitle("GSB : Frais de repas");
+
+        controleur = Controleur.getControleur();
+        datePicker = findViewById(R.id.datRepas);
+
         // modification de l'affichage du DatePicker
-        Outils.changeAfficheDate((DatePicker) findViewById(R.id.datRepas), false) ;
-        // valorisation des propriétés
-        valoriseProprietes() ;
+        Outils.changeAfficheDate(datePicker, false);
+
+        // valorisation de la quantité de frais saisis pour le mois sélectionné
+        controleur.valoriseProprietes(datePicker, typeFrais);
+        ((EditText) findViewById(R.id.txtRepas)).setText(String.format(Locale.FRANCE, "%d",
+                controleur.getQte()));
+
         // chargement des méthodes évènementielles
         onCreateListenersLoading();
     }
@@ -64,6 +76,28 @@ public class RepasActivity extends AppCompatActivity {
      */
     public void onCreateListenersLoading(){
         imgReturn_clic();
+        cmdValider_clic();
+        cmdPlus_clic();
+        cmdMoins_clic();
+        dat_clic();
+    }
+
+    /**
+     * Enregistrement dans la zone de texte et dans la liste de la nouvelle qte, à la date choisie
+     */
+    private void enregNewQte() {
+        Integer annee = controleur.getAnnee();
+        Integer mois = controleur.getMois();
+
+        // enregistrement dans la zone de texte
+        ((EditText) findViewById(R.id.txtRepas)).setText(String.format(Locale.FRANCE, "%d", controleur.getQte()));
+        // enregistrement dans la liste
+        Integer key = (annee * 100) + mois;
+        if (!Global.listFraisMois.containsKey(key)) {
+            // creation du mois et de l'annee s'ils n'existent pas déjà
+            Global.listFraisMois.put(key, new FraisMois(annee, mois));
+        }
+        Global.listFraisMois.get(key).setRepas(controleur.getQte());
     }
 
     /**
@@ -74,23 +108,59 @@ public class RepasActivity extends AppCompatActivity {
         startActivity(intent) ;
     }
 
-    /**
-     * Valorisation des propriétés avec les informations affichées
-     */
-    private void valoriseProprietes() {
-        annee = ((DatePicker)findViewById(R.id.datRepas)).getYear() ;
-        mois = ((DatePicker)findViewById(R.id.datRepas)).getMonth() + 1 ;
-        // récupération de la qte correspondant au mois actuel
-        qte = 0 ;
-        Integer key = annee*100+mois ;
-        if (Global.listFraisMois.containsKey(key)) {
-            qte = Global.listFraisMois.get(key).getRepas() ;
-        }
-        ((EditText)findViewById(R.id.txtRepas)).setText(String.format(Locale.FRANCE, "%d", qte)) ;
-    }
-
 
     // -------- EVENEMENTS --------
+    /**
+     * Sur le clic du bouton valider : sérialisation
+     */
+    private void cmdValider_clic() {
+        findViewById(R.id.cmdRepasValider).setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Serializer.serialize(Global.listFraisMois, RepasActivity.this);
+                retourActivityPrincipale();
+            }
+        });
+    }
+
+    /**
+     * Sur le clic du bouton plus : ajout de 10 dans la quantité
+     */
+    private void cmdPlus_clic() {
+        findViewById(R.id.cmdRepasPlus).setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                controleur.majQte("plus");
+                enregNewQte();
+            }
+        });
+    }
+
+    /**
+     * Sur le clic du bouton moins : soustrait 10 à la quantité si elle est >= 10
+     */
+    private void cmdMoins_clic() {
+        findViewById(R.id.cmdRepasMoins).setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                controleur.majQte("moins");
+                enregNewQte();
+            }
+        });
+    }
+
+    /**
+     * Sur le changement de date : mise à jour de l'affichage de la qte
+     */
+    private void dat_clic() {
+        datePicker.init(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth(),
+                new DatePicker.OnDateChangedListener() {
+                    @Override
+                    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        controleur.valoriseProprietes(datePicker, typeFrais);
+                        ((EditText) findViewById(R.id.txtRepas)).setText(String.format(Locale.FRANCE,
+                                "%d", controleur.getQte()));
+                    }
+                });
+    }
+
     /**
      * Sur la selection de l'image : retour au menu principal
      */
